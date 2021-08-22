a3ea5a2c327403a81ce002c064acc8af379f272f
Egemen <egemenongel@hotmail.com>
2021-08-22 20:39:21 +0300

    Accounts can be added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import './AddAccount/add_account.dart';
 import 'package:flutter/material.dart';
 import './HomePage/home_page.dart';
+import 'models/account_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,7 +42,9 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: HomePage(),
+      home: HomePage(
+        accountInfo: "",
+      ),
     );
   }
 
